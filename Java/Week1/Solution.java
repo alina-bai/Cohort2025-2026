5333ce29d026602bea4b1c0dd349b9211328943d
Alina-bai <alina.baishukurova@gmail.com>
2025-04-13 14:13:18 +0400

    Week 1 HackerRank Solutions

---
+++ Java/Week1/Solution.java
@@ -0,0 +1,28 @@
+package Week1;
+
+import java.io.*;
+import java.util.*;
+
+    public class Solution {
+
+        public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT.
+            Your class should be named Solution. */
+            Scanner scanner = new Scanner(System.in);
+            int myInt = scanner.nextInt();
+            int a = 42;
+            int b = 100;
+            int c = 125;
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+
+
+        }
+    }
+
+
+
+
+
