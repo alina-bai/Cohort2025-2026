938ca747b465e687401b01fb8c87481fdc6a12f4
Alina-bai <alina.baishukurova@gmail.com>
2025-04-16 15:11:35 +0400

    Week 1 HackerRank  Solution6

---
+++ Java/Week1/Solution4.java
@@ -0,0 +1,18 @@
+package Week1;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i =1; i<=10; i++){
+            int result = N*i;
+            System.out.println(N+ " x "+i+" = "+ result);
+        }
+        scanner.close();
+    }
+}
+
+
