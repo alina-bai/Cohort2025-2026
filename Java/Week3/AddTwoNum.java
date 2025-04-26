7e22357ca4b026332d7deb77fc37ecb55737bc99
Alina-bai <alina.baishukurova@gmail.com>
2025-04-26 08:52:19 +0400

    Week 3 Homework AddTwoNumbers

---
+++ Java/Week3/AddTwoNum.java
@@ -0,0 +1,15 @@
+package Week3;
+
+ class addTwoNum {
+     public static void main(String[] args) {
+        int a = 55;
+        int b = 65;
+        int result = addTwoNumbers(a,b);
+        System.out.println(result);
+
+    }
+    public static int addTwoNumbers(int a, int b) {
+        // Your code here
+        return  a+b;
+    }
+}
