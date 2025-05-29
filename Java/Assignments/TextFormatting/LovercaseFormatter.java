2a298e3db5334fa3c4f1a39e79cc74685d1af64e
Alina-bai <alina.baishukurova@gmail.com>
2025-05-29 14:25:51 +0400

    Assignment TextFormatting

---
+++ Java/Assignments/TextFormatting/LovercaseFormatter.java
@@ -0,0 +1,10 @@
+package Assignments.TextFormatting;
+
+public class LovercaseFormatter implements Formatter{
+    @Override
+    public String format (String input) {
+        return (input.toLowerCase());
+    }
+
+
+}
