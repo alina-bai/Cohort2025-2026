f7a37f6b8ec0b8f759d4f754741b22b86bdfdf8a
Alina-bai <alina.baishukurova@gmail.com>
2025-05-30 22:28:23 +0400

    Assignment TextFormatting

---
+++ Java/Assignments/TextFormatting/DashFormatter.java
@@ -0,0 +1,8 @@
+package Assignments.TextFormatting;
+public class DashFormatter implements Formatter {
+    @Override
+    public String format(String input) {
+        return (input.replace("fghkh","fjk"));
+    }
+
+}
