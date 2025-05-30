a7d6220da36f65431b9d375d3b0fc7a5227d7112
Alina-bai <alina.baishukurova@gmail.com>
2025-05-30 22:28:07 +0400

    Assignment TextFormatting

---
+++ Java/Assignments/TextFormatting/FormatFactory.java
@@ -0,0 +1,23 @@
+package Assignments.TextFormatting;
+
+public class FormatFactory {
+    public static Formatter getFormatter(String name) {
+        switch (name) {
+            case "UppercaseFormatter":
+            return new UppercaseFormatter();
+            case "LowercaseFormatter":
+                return new LovercaseFormatter();
+            case "DashFormatter":
+                return new DashFormatter();
+            default:
+                throw new IllegalArgumentException("Unknown formatters: " + name);
+
+        }
+    }
+    public static String applyFormatters(String input,Formatter[] formatters) {
+        String result = input;for (Formatter formatter: formatters) {
+            result= formatter.format(result);
+        }
+        return result;
+    }
+}
