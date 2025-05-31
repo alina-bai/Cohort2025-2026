6776d15f987ec24d37fdf56e0d7c4cfddbce3637
Alina-bai <alina.baishukurova@gmail.com>
2025-05-31 12:45:16 +0400

    informatics Gipotenuza2936

---
+++ Java/Informatics/Gipotenuza2936.java
@@ -0,0 +1,18 @@
+package Informatics;
+
+import java.util.Scanner;
+
+public class Gipotenuza2936 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println(c);
+
+
+    }
+}
