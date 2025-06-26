92e7735b7deefce253addcd23278027dfd22acd9
Alina-bai <alina.baishukurova@gmail.com>
2025-06-26 22:51:54 +0600

    Mentorship practicals. Bank Account

---
+++ MentorshipPracticals/BankAccount.java
@@ -0,0 +1,37 @@
+//Exercise 5: Basic class and Encapsulation
+public class BankAccount {
+ final String accountNumber;
+ double balance;
+ String ownerName;
+
+public BankAccount (String accountNumber, String ownerName, double balance){
+    this.accountNumber = accountNumber;
+    this.ownerName = ownerName;
+    this.balance = Math.max(0, balance);
+}
+public String getAccountNumber(){
+    return accountNumber;
+}
+public double getBalance(){
+    return balance;
+}
+public String getOwnerName(){
+    return ownerName
+}
+public void setOwnerName(String ownerName) {
+    this.ownerName = ownerName;
+}
+public void deposit (double amount) {
+    if(amount > 0 ) {
+        balance += amount;
+    }
+}
+public  void withdraw (double amount) {
+    if (amount > 0 && amount <= balance) {
+        balance -= amount;
+    }
+}
+
+
+
+
