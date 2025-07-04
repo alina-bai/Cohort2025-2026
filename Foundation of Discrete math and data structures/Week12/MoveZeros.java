21560177c90ff2601d8ce3ce322e094935dee9a2
Alina-bai <alina.baishukurova@gmail.com>
2025-07-04 19:57:44 +0400

    Week12 Problem 2 (#283)

---
+++ Foundation of Discrete math and data structures/Week12/MoveZeros.java
@@ -0,0 +1,17 @@
+package Week12;
+
+class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int insertPos=0;
+        for(int num : nums) {
+            if (num !=0){
+                nums [insertPos] = num;
+                insertPos++;
+            }
+        }
+        while (insertPos < nums.length) {
+            nums [insertPos] = 0;
+            insertPos++;
+        }
+    }
+}
