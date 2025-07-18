2e3430157399264e97a7eea0e1c27d6327b62ab4
Alina-bai <alina.baishukurova@gmail.com>
2025-07-18 17:27:33 +0400

    StreamAPIAdvanced

---
+++ MentorshipPracticals/StreamAPIAdvanced.java
@@ -0,0 +1,16 @@
+public class StreamAPIAdvanced {
+    private final String name;
+    private  final int age;
+    private final String city;
+
+    public Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city;
+    }
+
+    public String getName() {return name;}
+    public int getAge() {return age;}
+    public String getCity() {return city; }
+
+}
