0449eb0d52ec74abe4f9227b074fe1810621652c
Alina-bai <alina.baishukurova@gmail.com>
2025-07-19 19:00:44 +0400

    StreamAPIAdvanced(added)

--- MentorshipPracticals/StreamAPIAdvanced.java
+++ MentorshipPracticals/StreamAPIAdvanced.java
@@ -1,3 +1,10 @@
+//Exercise 14: Stream API -Advanced with Collectors
+
+import javax.xml.namespace.QName;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StreamAPIAdvanced {
     private final String name;
     private  final int age;
@@ -12,5 +19,16 @@ public class StreamAPIAdvanced {
     public String getName() {return name;}
     public int getAge() {return age;}
     public String getCity() {return city; }
+}
+@Override
+public String toString() {
+    return String.format("Person{name='%s', age=%d, age=%d, city='%s'}"),name, age, city);
+}
+}
 
+public static Map<String, Double> calculateAverageAgeByCity(List<Person> people) {
+    return people.stream()
+            .collect(Collectors.groupingBy(
+                    Person::getCity,
+                    Collectors.avaragingInt(Person::getAge)));
 }
