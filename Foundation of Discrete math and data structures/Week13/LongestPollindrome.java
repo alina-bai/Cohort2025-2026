dec92851a18d8bf6846b55d9203ee551d54ec38f
Alina-bai <alina.baishukurova@gmail.com>
2025-07-21 19:35:47 +0400

    LongestPollindrome

---
+++ Foundation of Discrete math and data structures/Week13/LongestPollindrome.java
@@ -0,0 +1,29 @@
+package Week13;
+
+import java.util.HashMap;
+
+class LongestPollindrome {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128]; // массив частот символов (по ASCII)
+
+        for (char c : s.toCharArray()) {
+            count[c]++;
+        }
+
+        int length = 0;
+        boolean hasOdd = false;
+
+        for (int freq : count) {
+            length += (freq / 2) * 2;
+            if (freq % 2 == 1) {
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd) {
+            length += 1; // можно добавить 1 символ в центр
+        }
+
+        return length;
+    }
+}
