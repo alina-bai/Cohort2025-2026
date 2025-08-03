1684f969bd1d41a822bd93936e8860469a1f5591
Alina-bai <alina.baishukurova@gmail.com>
2025-08-03 18:03:22 +0400

    HackerRank Easy level Regex

---
+++ Java/HackerRankSolutions/EasyRegex.java
@@ -0,0 +1,26 @@
+package HackerRankSolutions;
+
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class EasyRegex {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        while (testCases-- > 0) {
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch
+                (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+        }
+
+    }
+
+}
