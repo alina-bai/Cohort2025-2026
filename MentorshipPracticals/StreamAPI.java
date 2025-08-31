2555ee81278621c08fb9f079e94d9518b0a32026
Alina-bai <alina.baishukurova@gmail.com>
2025-08-31 15:55:22 +0400

    StreamAPI.java??

--- MentorshipPracticals/StreamAPI.java
+++ MentorshipPracticals/StreamAPI.java
@@ -1,14 +1,14 @@
-import java.util.Collection;
-import java.util.List;
-
-//Exercise 13: Stream Api - Basic Operations
-public class StreamAPI {
-    public  static List<Integer> processNumbers(List<Integer> numbers) {
-        return numbers.stream()
-                .filter(Integer n -> n % 2 ==0)
-                .map(Integer n -> n*n)
-                .sorted(Collection.reverseOrder())
-                .collect(Collection.toList());
-    }
-
-}
+//import java.util.Collection;
+//import java.util.List;
+//
+////Exercise 13: Stream Api - Basic Operations
+//public class StreamAPI {
+//    public  static List<Integer> processNumbers(List<Integer> numbers) {
+//        return numbers.stream()
+//                .filter(Integer n -> n % 2 ==0)
+//                .map(Integer n -> n*n)
+//                .sorted(Collection.reverseOrder())
+//                .collect(Collection.toList());
+//    }
+//
+//}
