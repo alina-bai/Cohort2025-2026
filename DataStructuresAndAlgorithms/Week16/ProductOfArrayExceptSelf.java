b108ad8d9b5a2f0e4eca5200fb52c907eff0cf2b
Alina-bai <alina.baishukurova@gmail.com>
2025-09-18 23:23:15 +0400

    Week 16 ProductOfArrayExceptSelf

---
+++ DataStructuresAndAlgorithms/Week16/ProductOfArrayExceptSelf.java
@@ -0,0 +1,44 @@
+package DataStructuresAndAlgorithms.Week16;
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int [] answer = new int[n];
+
+        int prefix = 1;
+        for (int i = 0; i< n; i++) {
+            answer[i]= prefix;
+            prefix*=nums[i];
+        }
+
+        int suffix = 1;
+        for (int i=n-1; i>=0; i--) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        ProductOfArrayExceptSelf solver = new ProductOfArrayExceptSelf();
+
+        int[] nums = {1,2,3,4};
+        int[]result = solver.productExceptSelf(nums);
+
+        System.out.print("Final answer:  [");
+        for (int i=0; i < result.length; i++) {
+            System.out.print(result[i]);
+            if (i < result.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+
+    }
+
+}
+
+
