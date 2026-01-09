336c0489a9c369c958fe3bea7b70e5f7ee2995b5
Alina-bai <alina.baishukurova@gmail.com>
2026-01-09 22:15:38 +0400

    FindMostFrequentCharacter.java

--- MentorshipPracticals/FindMostFrequentCharacter.java
+++ MentorshipPracticals/FindMostFrequentCharacter.java
@@ -1,29 +1,29 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static java.util.Collections.max;
-
-public class FindMostFrequentCharacter {
-    //#10 HashMap Operations
-    public static Character findMostFrequentCharacter(String input) {
-        if (input == null || input.isEmpty()) {
-            return null;
-        }
-        Map<Character, Integer> frequency = new HashMap<>();
-        for (char c : input.toCharArray()) {
-            // Integer value;
-            frequency.merge(c, value:1, Integer::sum);
-        }
-        return frequency.entrySet().stream() Stream < Entry <...>>
-    .max(Map.Entry.comparingByValue()) Optional < Entry <...>>
-    .map(Map.Entry::getKey) Optional<Character>
-        orElse(other:null);
-    }
-}
- //#11 GEneric Methods and WildCards
-public static double sumNumbers(List<? extends Number>numbers) {
-    return numbers.stream()Stream<capture of extends Number>
-    .mapToDouble(Number::doubleValue)DoubleStream
-            .sum();
-}
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//import static java.util.Collections.max;
+//
+//public class FindMostFrequentCharacter {
+//    //#10 HashMap Operations
+//    public static Character findMostFrequentCharacter(String input) {
+//        if (input == null || input.isEmpty()) {
+//            return null;
+//        }
+//        Map<Character, Integer> frequency = new HashMap<>();
+//        for (char c : input.toCharArray()) {
+//            // Integer value;
+//            frequency.merge(c, value:1, Integer::sum);
+//        }
+//        return frequency.entrySet().stream() Stream < Entry <...>>
+//    .max(Map.Entry.comparingByValue()) Optional < Entry <...>>
+//    .map(Map.Entry::getKey) Optional<Character>
+//        orElse(other:null);
+//    }
+//}
+// //#11 GEneric Methods and WildCards
+//public static double sumNumbers(List<? extends Number>numbers) {
+//    return numbers.stream()Stream<capture of extends Number>
+//    .mapToDouble(Number::doubleValue)DoubleStream
+//            .sum();
+//}
